679bb48f8b10081e2eb2ebe34466f853c5291c9e
Parthkumar2005 <1226parthkumar@gmail.com>
2025-01-26 19:55:46 +0530

    Add files via upload
    Q1) Write a program to demonstrate basic data type in python

---
+++ program4.py
@@ -0,0 +1,15 @@
+integer = 10
+print("Integer:", integer)
+print("Type of variable is integer:", type(integer))
+
+float= 10.5
+print("\nFloat:", float)
+print("Type of variable is float:", type(float))
+
+string = "Hello, Python!"
+print("\nString:", string)
+print("Type of variable is string:", type(string))
+
+boolean= True
+print("\nBoolean datatype is true or false like:", boolean)
+print("Type of boolean:",type(boolean))
